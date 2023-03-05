5f888f27f2e17ac6c0c8d3f7fb5a0fc203a813c2
Tifoysal <foysal11203004@gmail.com>
2023-03-05 11:56:22 +0600

    upate

--- app/Http/Controllers/PermissionController.php
+++ app/Http/Controllers/PermissionController.php
@@ -8,11 +8,8 @@ use Illuminate\Support\Facades\Route;
 
 class PermissionController extends Controller
 {
-<<<<<<< HEAD
 
-=======
     public function list(){
         return view('backend.pages.permission.index');
     }
->>>>>>> eaa6c6cdd28c278d9f9fdd8476d9d5f5f0cd1a66
 }
